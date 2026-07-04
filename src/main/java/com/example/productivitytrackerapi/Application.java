09753af5299de3c830d6fc4a71fb0f6f8aaf126b
alex-softuni <notorious.atanasow@gmail.com>
2026-07-04 22:51:50 +0300

    chore: initialize project with dependencies

--- src/main/java/com/example/productivitytrackerapi/Application.java
+++ src/main/java/com/example/productivitytrackerapi/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductivityTrackerApiApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProductivityTrackerApiApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
